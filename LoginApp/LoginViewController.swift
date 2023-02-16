92149604c5ba4d20c3697322f599874e9a871eb3
romkalan <romkalan@mail.ru>
2023-02-16 12:02:52 +0300

    Creation segue With identifier correction logic code with LogIn Button (add preferSegue(withIdentifier:)

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -13,19 +13,19 @@ final class LoginViewController: UIViewController {
     @IBOutlet var passwordTF: UITextField!
     @IBOutlet var logInButton: UIButton!
     
-    let userName = "Alexey"
-    let password = "123"
+    private let userName = "User"
+    private let password = "123"
     
     override func viewDidLoad() {
+        userNameTF.text = userName
+        passwordTF.text = password
         super.viewDidLoad()
         logInButton.layer.cornerRadius = 10
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let welcomeVC = segue.destination as? WelcomeViewController else {
-            return
-        }
-        welcomeVC.userName = userNameTF.text
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userName
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -34,20 +34,18 @@ final class LoginViewController: UIViewController {
     }
 
     @IBAction func logInButtonPressed() {
-        if userName != userNameTF.text || password != passwordTF.text {
-            showAlert(
-                withTitle: "Invalid Login or Password",
-                andMessage: "Please, enter correct login and password"
-            )
-        }
+        userName == userNameTF.text || password == passwordTF.text
+        ? performSegue(withIdentifier: "openWelcomeVC", sender: nil)
+        : showAlert(
+            withTitle: "Invalid Login or Password",
+            andMessage: "Please, enter correct login and password"
+        )
     }
     
-    @IBAction func forgotNameButtonPressed() {
-        showAlert(withTitle: "Oops!", andMessage: "Your name is Alexey 🥸")
-    }
-    
-    @IBAction func forgotPasswordButtonPressed() {
-        showAlert(withTitle: "Oops!", andMessage: "Your password is 123 🥸")
+    @IBAction func forgotRegisterData(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(withTitle: "Oops!", andMessage: "Your name is \(userName) 🥸")
+            : showAlert(withTitle: "Oops!", andMessage: "Your password is \(password) 🥸")
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
@@ -59,15 +57,10 @@ final class LoginViewController: UIViewController {
 // MARK: - UIAlertController
 extension LoginViewController {
     private func showAlert(withTitle title: String, andMessage message: String) {
-        let alert = UIAlertController(
-            title: title,
-            message: message,
-            preferredStyle: .alert
-        )
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
                 self.passwordTF.text = ""
             }
-        
         alert.addAction(okAction)
         present(alert, animated: true)
     }
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -8,27 +8,28 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet var userNameLabel: UILabel!
-    var userName: String!
+    var userName = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        userNameLabel.text = "Welcome, \(userName ?? "")!"
-        setGradientBackground(colorTop: .systemPink, andColorBottom: .systemBlue)
+        userNameLabel.text = "Welcome, \(userName)!"
+        view.setGradientBackground(colorTop: .systemPink, colorBottom: .systemBlue)
     }
-    
-    private func setGradientBackground(colorTop firstColor: UIColor, andColorBottom secondColor: UIColor) {
-        let gradientLayer = CAGradientLayer()
-        
-        let colorTop =  firstColor
-        let colorBottom = secondColor
-        
-        gradientLayer.colors = [colorTop.cgColor, colorBottom.cgColor]
-        gradientLayer.frame = view.bounds
-        gradientLayer.shouldRasterize = true
-        view.layer.addSublayer(gradientLayer)
-        view.layer.insertSublayer(gradientLayer, at:0)
-    }
-
 }
+
+// MARK: - Set background color
+extension UIView {
+    func setGradientBackground(colorTop: UIColor, colorBottom: UIColor) {
+        let gradientLayer = CAGradientLayer()
+            
+        gradientLayer.colors = [colorTop.cgColor, colorBottom.cgColor]
+        gradientLayer.frame = bounds
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradientLayer, at:0)
+    }
+}
+
